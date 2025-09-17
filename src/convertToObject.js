eb8ece4a5d18da378f58695c5bc6c5b178ac82ba
Valentyn <v.lizun98@gmail.com>
2025-09-17 18:21:15 +0300

    add task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,21 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
-}
+  const styleSheet = {};
 
+  const input = sourceString.split(';');
+
+  const inputTrimmed = input.map((item) => item.trim());
+  const inputFiltered = inputTrimmed.filter((item) => item.length >= 1);
+
+  inputFiltered.forEach((item) => {
+    const coloneIndex = item.indexOf(':');
+    const key = item.slice(0, coloneIndex).trim();
+    const value = item.slice(coloneIndex + 1).trim();
+
+    styleSheet[key] = value;
+  });
+
+  return styleSheet;
+}
 module.exports = convertToObject;
